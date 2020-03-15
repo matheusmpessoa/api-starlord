7152f172898d41b8e8de74dde32bbec1db9c2ae7
Matheus Pessoa <matheusmpessoa17@gmail.com>
2020-03-15 19:26:52 -0300

    Create service and table of api-starlord

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarlordService } from './starlord.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'starlord';
+export class AppComponent implements OnInit {
+  messageError: string;
+  createReqStarlordService: any;
+  responseObj: Array<{}>;
+
+  constructor(
+    public starlordService: StarlordService,
+  ) { }
+
+  ngOnInit() {
+    this.createListStarlord();
+  }
+
+  ngOnDestroy() {
+    this.createReqStarlordService.unsubscribe();
+  }
+
+  createListStarlord() {
+    this.createReqStarlordService = this.starlordService.getStarlord()
+      .subscribe(
+        response => {
+          if (response) {
+            console.log(response);
+            this.responseObj = response;
+          }
+        },
+        error => {
+          this.messageError = "Error. Try again";
+        }
+      )
+  }
 }
